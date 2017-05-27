2efe8b743001f6c4d81b5a0efb2bdfb7bc79e0a8
ChristianHarten <christianharten23@gmail.com>
2017-05-27 21:33:14 +0200

    Service with Thread Aufgabe a

--- PlayerService/app/src/main/java/de/hochschule_trier/playerservice/ServiceThread.java
+++ PlayerService/app/src/main/java/de/hochschule_trier/playerservice/ServiceThread.java
@@ -1,29 +1,34 @@
 package de.hochschule_trier.playerservice;
 
+import android.os.Process;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by eschs on 27.05.2017.
  */
 
 public class ServiceThread extends Thread {
-    private int count;
+    private int count, i;
 
     public ServiceThread (int count) {
         this.count = count;
+        this.i = 0;
     }
     @Override
     public void run() {
-        int i = 0;
         try {
             while (i < count) {
                 i++;
                 Log.d(this.getClass().getName(), "service with thread " + android.os.Process.myPid() + " - " + Thread.currentThread().getName() + ", iteration " + i);
-                Thread.sleep(2000);
+                Thread.sleep(Toast.LENGTH_LONG);
             }
         }
         catch (Exception e) {
 
         }
+    }
+    public void setCount(int newCount) {
+        this.count = i < newCount ? newCount : 0;
     }
 }
